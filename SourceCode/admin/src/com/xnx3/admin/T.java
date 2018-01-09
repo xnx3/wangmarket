1c3e611e43088c034fd3cd04c9b7ab61c379419e
xnx3 <mail@xnx3.com>
2018-01-09 19:22:56 +0800

    xnx3
    update

--- SourceCode/admin/src/com/xnx3/admin/T.java
+++ SourceCode/admin/src/com/xnx3/admin/T.java
@@ -1,12 +1,18 @@
 package com.xnx3.admin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.xnx3.Lang;
+import com.xnx3.j2ee.Global;
+import com.xnx3.net.MailUtil;
 
 
 public class T {
+	
 	public static void main(String[] args){
 		
-		System.err.println(Lang.uuid().length());// 输出密钥  
+		
 	}
 	
 }
