6b2596bd205278bef3815cf13751515ae7d2f038
xnx3 <mail@xnx3.com>
2018-01-21 21:50:26 +0800

    wangmarket.zip
    v3.7升级说明
    http://www.wang.market/2617.html

--- SourceCode/admin/src/com/xnx3/admin/T.java
+++ SourceCode/admin/src/com/xnx3/admin/T.java
@@ -1,18 +1,9 @@
 package com.xnx3.admin;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.xnx3.Lang;
-import com.xnx3.j2ee.Global;
-import com.xnx3.net.MailUtil;
-
 
 public class T {
-	
 	public static void main(String[] args){
 		
 		
 	}
-	
 }
