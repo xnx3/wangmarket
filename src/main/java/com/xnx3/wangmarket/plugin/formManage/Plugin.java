59db6cbd41830d0bfeca72e588a1570d7ea707ed
xnx3 <mail@xnx3.com>
2018-10-19 12:32:21 +0800

    万能表单插件的 plugin 注册类

---
+++ src/main/java/com/xnx3/wangmarket/plugin/formManage/Plugin.java
@@ -0,0 +1,13 @@
+package com.xnx3.wangmarket.plugin.formManage;
+
+import com.xnx3.wangmarket.admin.pluginManage.anno.PluginRegister;
+
+/**
+ * 将此万能表单插件注册到网市场中的CMS模式网站管理后台中
+ * @author 管雷鸣
+ *
+ */
+@PluginRegister(id="formManage" , menuTitle = "表单反馈",menuHref="../../form/list.do", applyToCMS=true)
+public class Plugin{
+	
+}
