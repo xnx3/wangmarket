499db94b9907a4ad03b9a748bdae94e373e6c82d
xnx3 <mail@xnx3.com>
2018-11-02 10:13:11 +0800

    修复当代理商登陆网站后，再访问代理后台页面时，不再出现错误提示，而提示“403，无权使用”

--- src/main/java/com/xnx3/j2ee/system/ErrorConfigurar.java
+++ src/main/java/com/xnx3/j2ee/system/ErrorConfigurar.java
@@ -15,11 +15,12 @@ import org.springframework.stereotype.Component;
 public class ErrorConfigurar implements ErrorPageRegistrar{
 
 	public void registerErrorPages(ErrorPageRegistry registry) {
-		ErrorPage[] errorPages=new ErrorPage[3];
+		ErrorPage[] errorPages=new ErrorPage[4];
         errorPages[0]=new ErrorPage(HttpStatus.NOT_FOUND,"/404.do");
         errorPages[1]=new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,"/500.do");
         errorPages[2]=new ErrorPage(HttpStatus.NOT_ACCEPTABLE,"/406.do");
-
+        errorPages[3]=new ErrorPage(org.apache.shiro.authz.AuthorizationException.class, "/403.do");	//优先根据此来进行排查。 先根据具体异常的类、再根据错误码
+        
         registry.addErrorPages(errorPages);
 	}
 	
