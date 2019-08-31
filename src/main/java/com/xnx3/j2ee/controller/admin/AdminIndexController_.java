a8206a87eeafc7607886395a1ea972a80577df07
xnx3 <mail@xnx3.com>
2019-08-31 09:26:06 +0800

    总管理后台增加 jumpUrl 这个参数

--- src/main/java/com/xnx3/j2ee/controller/admin/AdminIndexController_.java
+++ src/main/java/com/xnx3/j2ee/controller/admin/AdminIndexController_.java
@@ -8,6 +8,7 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.xnx3.MD5Util;
 import com.xnx3.j2ee.Func;
@@ -29,10 +30,12 @@ public class AdminIndexController_ extends BaseController{
 	
 	/**
 	 * 管理后台首页
+	 * @param jumpUrl 这里在应用插件里面，安装插件后要刷新页面，所以加入了jumpUrl，传入加载地址，如果有jumpUrl，那么默认页面就是访问这个。这里传入的地址，如 plugin/pluginManage/index.do 
 	 */
 	@RequiresPermissions("adminIndexIndex")
 	@RequestMapping("index${url.suffix}")
-	public String index(HttpServletRequest request, Model model){
+	public String index(HttpServletRequest request, Model model,
+			@RequestParam(value = "jumpUrl", required = false , defaultValue="") String jumpUrl){
 		//登录成功后，管理后台的主题页面，默认首页的url
 		String url = "admin/index/welcome.do"; 
 		
@@ -40,6 +43,11 @@ public class AdminIndexController_ extends BaseController{
 		if(Func.isAuthorityBySpecific(getUser().getAuthority(), Global.get("ROLE_SUPERADMIN_ID"))){
 			//有超级管理员权限
 			url = "admin/index/welcome.do";
+			
+			//这里在应用插件里面，安装插件后要刷新页面，所以加入了jumpUrl，传入加载地址，如果有jumpUrl，那么默认页面就是访问这个。v4.11增加
+			if(jumpUrl.length() > 2){
+				url = jumpUrl;
+			}
 			
 			//获取网站后台管理系统有哪些功能插件，也一块列出来,以直接在网站后台中显示出来
 			String pluginMenu = "";
