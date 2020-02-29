6c5080252d895f2c55be8af1cbbd780a552982ed
xnx3 <mail@xnx3.com>
2020-02-29 08:54:00 +0800

    CacheUtil 注释优化

--- src/main/java/com/xnx3/j2ee/util/CacheUtil.java
+++ src/main/java/com/xnx3/j2ee/util/CacheUtil.java
@@ -18,7 +18,7 @@ public class CacheUtil {
 	
 	/**
 	 * 设置缓存
-	 * @param key 设置时，多个可以用.分隔开，就如java的包名一样。杜绝一个key对应的value过大的情况！一个value尽可能不要超过10KB
+	 * @param key 设置时，多个可以用英文字符:分隔开，就如 user:guanleiming   user:lixin  。同时杜绝一个key对应的value过大的情况！一个value尽可能不要超过10KB
 	 * @param value 缓存的值。坚决杜绝value过大，一个value尽可能不要超过10KB，如果太大，建议利用key进行拆分，如 key 为 user.1 存放用户编号为1的缓存信息
 	 */
 	public static void set(String key, Object value){
@@ -33,7 +33,7 @@ public class CacheUtil {
 	
 	/**
 	 * 设置缓存
-	 * @param key 设置时，多个可以用.分隔开，就如java的包名一样。杜绝一个key对应的value过大的情况！一个value尽可能不要超过10KB
+	 * @param key 设置时，多个可以用英文字符:分隔开，就如 user:guanleiming   user:lixin  。同时杜绝一个key对应的value过大的情况！一个value尽可能不要超过10KB
 	 * @param value 缓存的值。坚决杜绝value过大，一个value尽可能不要超过10KB，如果太大，建议利用key进行拆分，如 key 为 user.1 存放用户编号为1的缓存信息
 	 * @param expiretime 当前key-value的过期时间，单位是秒。比如设定为2，则超过2秒后没使用，会自动删除调。注意，只有使用redis时此参数才会有效
 	 */
