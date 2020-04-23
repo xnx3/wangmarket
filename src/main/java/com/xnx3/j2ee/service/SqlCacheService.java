fe2905bb7ccdbc0a164b39fc7405415329f18ef3
xnx3 <mail@xnx3.com>
2020-04-23 11:10:19 +0800

    SqlCacheService 增加 property 的缓存

--- src/main/java/com/xnx3/j2ee/service/SqlCacheService.java
+++ src/main/java/com/xnx3/j2ee/service/SqlCacheService.java
@@ -1,5 +1,7 @@
 package com.xnx3.j2ee.service;
 
+import java.util.List;
+
 import com.xnx3.j2ee.entity.User;
 
 /**
@@ -19,9 +21,33 @@ public interface SqlCacheService {
 	
 	/**
 	 * 删除缓存的记录。这里只是删除缓存的记录而已，并不会影响mysql表的信息。
-	 * 可以用来作为更新缓存使用
+	 * 可以用来作为更新缓存使用。
+	 * 这里删除的缓存，是 {@link #findById(Class, Object)} 中产生的缓存数据
 	 * @param entity 实体类，如 {@link User}.class
 	 * @param id 主键id，object
 	 */
-	public void deleteCache(Class entity, Object id);
+	public void deleteCacheById(Class entity, Object id);
+	
+	
+	/**
+	 * 根据字段名查一条值，取一条记录。
+	 * 1. value会自动进行sql注入过滤
+	 * 2. 会先从缓存中取，如果缓存中没有，再从mysql中取。取出来如果有值，放入缓存
+	 * @param entity {@link Class} 实体类，如 {@link User}.class
+	 * @param propertyName 数据表字段名(Hibernate 语句的字段名，驼峰命名,非数据库的字段名)
+	 * @param value 值
+	 * @return {@link List} 实体类
+	 */
+	public <E> E findAloneByProperty(Class<E> entity,String propertyName, Object value);
+	
+	/**
+	 * 删除缓存的记录。这里只是删除缓存的记录而已，并不会影响mysql表的信息。
+	 * 可以用来作为更新缓存使用。
+	 * 这里删除的缓存，是 {@link #findAloneByProperty(Class, String, Object)} 中产生的缓存数据
+	 * @param entity {@link Class} 实体类，如 {@link User}.class
+	 * @param propertyName 数据表字段名(Hibernate 语句的字段名，驼峰命名,非数据库的字段名)
+	 * @param value 值
+	 */
+	public void deleteCacheByProperty(Class entity,String propertyName, Object value);
+	
 }
--- src/main/java/com/xnx3/j2ee/service/impl/SqlCacheServiceImpl.java
+++ src/main/java/com/xnx3/j2ee/service/impl/SqlCacheServiceImpl.java
@@ -8,8 +8,10 @@ import com.xnx3.j2ee.util.CacheUtil;
 
 @Service("sqlCacheService")
 public class SqlCacheServiceImpl implements SqlCacheService {
-	//{entity}实体类的名字，如 user    {id}主键的内容，如 1
-	public static final String CACHE_KEY = "sql:{entity}:{id}";
+	//通过主键查询出来的实体类信息的缓存key， {entity}实体类的名字，如 user    {id}主键的内容，如 1
+	public static final String CACHE_KEY_BY_ID  = "sql:{entity}:id:{id}";
+	//通过指定字段查询出来的实体类信息的缓存key， {entity}实体类的名字，如 user   {property} 实体类中驼峰字段的名字，如 username   {value}查询的字段的值，如 1
+	public static final String CACHE_KEY_BY_PROPERTY  = "sql:{entity}:{property}:{value}";
 	
 	@Resource
 	private SqlDAO sqlDAO;
@@ -24,7 +26,7 @@ public class SqlCacheServiceImpl implements SqlCacheService {
 
 	@Override
 	public <E> E findById(Class<E> entity, Object id) {
-		String key = CACHE_KEY.replace("{entity}", entity.getName()).replace("{id}", id.toString());
+		String key = CACHE_KEY_BY_ID.replace("{entity}", entity.getName()).replace("{id}", id.toString());
 		//先从缓存中取，看缓存中有没有
 		E e = (E) CacheUtil.get(key);
 		if(e == null){
@@ -33,14 +35,35 @@ public class SqlCacheServiceImpl implements SqlCacheService {
 			if(e != null){
 				CacheUtil.setWeekCache(key, e);
 			}
-			System.out.println("chaxun");
 		}
 		return e;
 	}
 
 	@Override
-	public void deleteCache(Class entity, Object id) {
-		String key = CACHE_KEY.replace("{entity}", entity.getName()).replace("{id}", id.toString());
+	public void deleteCacheById(Class entity, Object id) {
+		String key = CACHE_KEY_BY_ID.replace("{entity}", entity.getName()).replace("{id}", id.toString());
+		CacheUtil.delete(key);
+	}
+
+
+	@Override
+	public <E> E findAloneByProperty(Class<E> entity, String propertyName, Object value) {
+		String key = CACHE_KEY_BY_PROPERTY.replace("{entity}", entity.getName()).replace("{property}", propertyName).replace("{value}", value.toString());
+		//先从缓存中取，看缓存中有没有
+		E e = (E) CacheUtil.get(key);
+		if(e == null){
+			//缓存中没有，那么从mysql中读
+			e = (E) sqlDAO.findAloneByProperty(entity, propertyName, value);
+			if(e != null){
+				CacheUtil.setWeekCache(key, e);
+			}
+		}
+		return e;
+	}
+
+	@Override
+	public void deleteCacheByProperty(Class entity, String propertyName, Object value) {
+		String key = CACHE_KEY_BY_PROPERTY.replace("{entity}", entity.getName()).replace("{property}", propertyName).replace("{value}", value.toString());
 		CacheUtil.delete(key);
 	}
 
