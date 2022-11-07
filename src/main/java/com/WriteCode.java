ca6203edd6dedf575e340a15cc7b685c6eb6b238
xnx3 <mail@xnx3.com>
2022-11-07 10:11:05 +0800

    增加 writecode.java

---
+++ src/main/java/com/WriteCode.java
@@ -0,0 +1,17 @@
+package com;
+
+import com.xnx3.writecode.template.wm.Code;
+
+/**
+ * 自动写代码，根据数据表写出实体类、controller、vo、jsp页面等
+ * 【注意，只支持Mysql数据库，暂不支持Sqlite】
+ * @author 管雷鸣
+ */
+public class WriteCode {
+	public static void main(String[] args) {
+		Code code = new Code();
+		code.setPackageName("com.xnx3.demo");	//设置生成的entity、controller、vo等java类放到哪个包下
+		code.setProjectUrlPath("/demo/"); //设置url请求的路径
+		code.write();	//执行
+	}
+}
