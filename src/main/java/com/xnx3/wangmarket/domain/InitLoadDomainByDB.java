9a82d1df9087eb9c655719b3398d4d9a3208034c
鬼画符 <mail@xnx3.com>
2024-07-09 20:22:40 +0800

    优化项目启动时，windows一件运行包默认域名有时不加载的问题

--- src/main/java/com/xnx3/wangmarket/domain/InitLoadDomainByDB.java
+++ src/main/java/com/xnx3/wangmarket/domain/InitLoadDomainByDB.java
@@ -16,44 +16,13 @@ import com.xnx3.wangmarket.domain.bean.SimpleSite;
  * @author 管雷鸣
  */
 @Component
-public class InitLoadDomainByDB {
-	public static boolean cache = false;	//是否已缓存，记录。若为true，则是已缓存
-	
-	public InitLoadDomainByDB() {
-		new Thread(new Runnable() {
-			
-			public void run() {
-				boolean b = true;
-				while(b){
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					if(SystemUtil.get("ATTACHMENT_FILE_MODE") == null || SystemUtil.get("ATTACHMENT_FILE_MODE").length() == 0){
-						//项目还未启动，system数据都还没有加载，继续等待
-					}else{
-						//system数据表数据已加载入内存，可以进行初始化域名数据了，退出等待
-						b = false;
-					}
-				}
-				
-				domainBind();
-			}
-		}).start();
-		
-	}
+public class InitLoadDomainByDB implements com.xnx3.j2ee.pluginManage.interfaces.DatabaseLoadFinishInterface{
 	
 	/**
 	 * 启动项目时，进行域名绑定，从数据库中取域名相关数据
 	 */
 	public void domainBind(){
-		if(cache){
-			//已缓存，无需再缓存了
-			return;
-		}
-		InitLoadDomainByDB.cache = true;
-		
+		ConsoleUtil.log(" domainBind start ... ");
 		List<Map<String, Object>> list = SpringUtil.getSqlService().findMapBySqlQuery("SELECT id,client,domain,bind_domain,state,template_id FROM site");
 		for (int i = 0; i < list.size(); i++) {
 			Map<String, Object> map = list.get(i);
@@ -82,34 +51,12 @@ public class InitLoadDomainByDB {
 			}
 		}
 		
-		
-		
-//		ResultSet rs;
-//		try {
-//			rs = com.xnx3.j2ee.func.DB.getResultSet("SELECT id,client,domain,bind_domain,state,template_id FROM site");
-//			//如果有这行存在
-//			while(rs.next()){
-//				SimpleSite ss = new SimpleSite();
-//				ss.setBindDomain(rs.getString("bind_domain"));
-//				ss.setClient(rs.getInt("client"));
-//				ss.setDomain(rs.getString("domain"));
-//				ss.setId(rs.getInt("id"));
-//				ss.setState(rs.getShort("state"));
-//				ss.setTemplateId(rs.getInt("template_id"));
-//				
-//				if(ss.getDomain() != null && ss.getDomain().length() > 0){
-//					G.putDomain(ss.getDomain(), ss);
-//				}
-//				if(ss.getBindDomain() != null && ss.getBindDomain().length() > 2){
-//					G.putBindDomain(ss.getBindDomain(), ss);
-//				}
-//			}
-//			
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-		
 		ConsoleUtil.info("共缓存二级域名："+G.getDomainSize()+"个， 绑定域名："+G.getBindDomainSize()+"个");
+	}
+
+	@Override
+	public void databaseLoadFinish() {
+		domainBind();
 	}
 	
 }
